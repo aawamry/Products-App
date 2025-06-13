4af5c5e897cb563981571bc97075d3034c5ad399
aawamry <a.awamry@live.com>
2025-06-13 13:24:34 +0300

    correcting file importing directory

--- routes/views/productviewroutes.js
+++ routes/views/productviewroutes.js
@@ -1,18 +1,14 @@
 import express from 'express';
-import { getAllProducts } from '../../controllers/productcontroller.js';
+import {
+	getAllProducts,
+	renderAddProductForm,
+	renderEditProductForm
+} from '../../controllers/views/productviewcontroller.js';
 
 const router = express.Router();
 
-router.get('/', getAllProducts); // renders EJS template
-
-router.get('/add', (req, res) => { // renders EJS template for adding a new product
-	res.render('products/form', {
-		title: 'Add Product',
-		formAction: '/api/products',
-		method: 'POST',
-		product: null,
-	});
-});
-
+router.get('/', getAllProducts);             // View all products
+router.get('/add', renderAddProductForm);    // Render "Add Product" form
+router.get('/:id/edit', renderEditProductForm); // Render "Edit Product" form
 
 export default router;
